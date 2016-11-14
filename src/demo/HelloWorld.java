4a705241f78e05a0f63596158f9ed18049bc9edb
yuanxin <154405A@mymail.nyp.edu.sg>
2016-11-14 15:08:34 +0800

    Initial commit

---
+++ src/demo/HelloWorld.java
@@ -0,0 +1,17 @@
+package demo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+/**
+ * Created by Owner on 11/14/2016.
+ */
+@Path("/hello")
+public class HelloWorld {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessage() {
+        return "Hello world!";
+    }
+}
